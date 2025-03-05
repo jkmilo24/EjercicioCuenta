97cd353a26471f6831034570661fc6bbb0004720
erfiliovargas <windowsevolucion78@gmail.com>
2025-03-04 20:29:23 -0500

    ejemplo de uso de git en netbeans

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -16,7 +16,7 @@ public class Ejercicio1 {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo2");
     }
     
 }
